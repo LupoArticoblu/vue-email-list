69c19951e33f2d805f1fd99db9d231e7729733c7
LupoArticoblu <wolf94-@libero.it>
2022-11-11 17:03:26 +0100

    inserimento methods

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ createApp({
     return{
       messaggio:'ciau'
     }
+  },
+  methods:{
+    getApi(){
+      mail.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    }
   }
 }
 ).mount('#app');
