6b868a8f50d111f111db1dffcfa51c984ac10451
Lorenzo C <93392246+LupoArticoblu@users.noreply.github.com>
2022-11-18 17:00:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-      messaggio:'ciau'
+      title: 'Axios Mail',
+      emailList: []
     }
   },
   methods:{
     getApi(){
-      mail.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      for (let i = 1; i <= 10; i++){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(result => {
+        this.emailList.push(result.data.response)
+     })
     }
+   }   
   }
-}
 ).mount('#app');
